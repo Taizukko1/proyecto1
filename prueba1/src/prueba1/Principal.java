b72b61e74b060493876b97bb1160f0d84e38f99f
Andoni <andsanace@gmail.com>
2024-03-05 14:04:22 +0100

    Segunda validacion de Principal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,6 +3,10 @@ package prueba1;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
+		System.out.println("If it hadn't been for Cotton-Eye Joe\r\n"
+				+ "I'd been married long time ago\r\n"
+				+ "Where did you come from, where did you go?\r\n"
+				+ "Where did you come from, Cotton-Eye Joe?");
 
 	}
 }
